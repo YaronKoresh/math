1d6b91e641bdece4f6258ba25c6fb59307e6bc3f
Yaron Koresh <aharonkoresh1@gmail.com>
2023-10-20 23:02:19 +0300

    Add files via upload

--- index.mjs
+++ index.mjs
@@ -239,11 +239,10 @@ var Divide = function(num1, num2, precision = "6") {
     let loopValue = "0";
     let rounds = "0";
     let multi = Math.max(ret3.split(".")[0].length - num.split(".")[0].length - 1, 0);
+    let floatLen = num.indexOf(".");
     let roundStepSize = "1" + "0".repeat(multi);
-    let valueStepSize = num;
-    for (let i = "1"; Greater(i, roundStepSize) === roundStepSize; i = Add(i, 1)) {
-      valueStepSize = Add(valueStepSize, num);
-    }
+    floatLen = floatLen === -1 ? 0 : num.length - floatLen - 1;
+    let valueStepSize = floatLen === 0 ? num + "0".repeat(multi) : floatLen <= multi ? num.replace(".", "") + "0".repeat(multi - floatLen) : num.replace(".", "").slice(0, num.replace(".", "").length - floatLen + multi) + "." + num.replace(".", "").slice(num.replace(".", "").length - floatLen + multi);
     while (true) {
       let nextStep = Add(loopValue, valueStepSize);
       let condition = Greater(nextStep, ret3);
@@ -260,7 +259,9 @@ var Divide = function(num1, num2, precision = "6") {
         }
         multi -= 1;
         roundStepSize = "1" + "0".repeat(multi);
-        valueStepSize = num + "0".repeat(multi);
+        floatLen = valueStepSize.indexOf(".");
+        floatLen = floatLen === -1 ? 0 : valueStepSize.length - floatLen - 1;
+        valueStepSize = floatLen === 1 && valueStepSize.slice(valueStepSize.length - 1) === "0" ? valueStepSize.slice(0, valueStepSize.length - 2) : valueStepSize.slice(valueStepSize.length - 1) === "0" ? valueStepSize.slice(0, valueStepSize.length - 1) : valueStepSize.replace(".", "").slice(0, valueStepSize.replace(".", "").length - floatLen - 1) + "." + valueStepSize.replace(".", "").slice(valueStepSize.replace(".", "").length - floatLen - 1);
       }
     }
     if (loopValue !== ret3) {
@@ -295,13 +296,26 @@ var Divide = function(num1, num2, precision = "6") {
     }
     return (negative ? "-" : "") + rounds;
   };
+  while (ret.slice(".")[0].length > 1 && ret.slice(0, 1) === "0") {
+    ret = ret.slice(1);
+  }
+  while (ret.includes(".") && ret.slice(ret.length - 1) === "0") {
+    ret = ret.slice(0, ret.length - 1);
+  }
   const Calc = function(num) {
+    num = num.toString();
+    while (num.slice(".")[0].length > 1 && num.slice(0, 1) === "0") {
+      num = num.slice(1);
+    }
+    while (num.includes(".") && num.slice(num.length - 1) === "0") {
+      num = num.slice(0, num.length - 1);
+    }
     ret = _Divide(ret, num);
   };
   nums.map((num) => Calc(num));
   let ret1 = ret.split(".")[0] ?? "0";
   let ret2 = ret.split(".")[1] ?? "0";
-  while (ret2.slice(ret2.length - 1) === "0" && ret2.length > 0) {
+  while (ret2.length > 0 && ret2.slice(ret2.length - 1) === "0") {
     ret2 = ret2.slice(0, ret2.length - 1);
   }
   if (ret2.length === 0) {
